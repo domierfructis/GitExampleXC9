e63221a0732bd468873ca5cac0556f193fb4fbb7
Dom Gordon <dominicklgordon@gmail.com>
2018-08-05 23:36:32 -0700

    Initial Commit

---
+++ GitExampleXC9/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  GitExampleXC9
+//
+//  Created by dom on 8/5/18.
+//  Copyright © 2018 Dom Gordon. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ GitExampleXC9/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GitExampleXC9
+//
+//  Created by dom on 8/5/18.
+//  Copyright © 2018 Dom Gordon. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
